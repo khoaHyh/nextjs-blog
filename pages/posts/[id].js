7d44796279e80a1fbe6934887818d8e72cee161c
khoaHyh <khoahuynh.toronto@gmail.com>
2021-01-24 15:21:34 -0500

    Feat: Implement getStaticProps
    Add getStaticProps function and successfully generate dynamic routes. This function fetches necessary data to render the post with the given `id`.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,16 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
-const Post = () => {
-  return <Layout>...</Layout>
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
 export const getStaticPaths = async() => {
@@ -16,6 +24,12 @@ export const getStaticPaths = async() => {
 
 export const getStaticProps = ({ params }) => {
     // Fetch necessary data for the blog post using params.id
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    }
 }
 
 export default Post;
